5714942f49dd6012c3d29c622d9203828e77b404
exyzte <davidecicerys@gmail.com>
2025-08-12 14:01:39 -0400

    completed functions for the game to work

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -27,3 +27,63 @@ function shuffle(array) {
     }
     return array;
 }
+
+function handleClickEvent(event) {
+    const card = event.target;
+    if(!card.classList.contains('card') || card.classList.contains('matched')) {
+        return;
+    }
+    card.textContent = card.dataset.color;
+    card.style.backgroundColor = card.dataset.color;
+    selectedCards.push(card);
+    if (selectedCards.length === 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    const [card1, card2] = selectedCards;
+    if (card1.datase.color === card2.dataset.color) {
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        score += 2;
+        scoreElement.textContent = `Score: ${score}`;
+
+    } else {
+        card1.textContent = '?';
+        card2.textContent = '?';
+        card1.style.backgroundColor = '#ddd';
+        card1.style.backgroundColor = '#ddd';
+    }
+
+    selectedCards = [];
+}
+
+function startGame() {
+    let timeLeft = 30;
+    startbtn.disabled = true;
+    score = 0;
+    scoreElement.textContent = `Score: ${score}`;
+    startGameTimer(timeLeft);
+    cards = shuffle(colors.concat(colors));
+    selectedCards = [];
+    gameContainer.innerHTML = '';
+    generateCards();
+    gameContainer.addEventListener('click', handleCardClick);
+}
+
+function startGameTimer(timeLeft) {
+    timerElement.textContent = `Time left: ${timeLeft}`;
+    gameInterval = setInterval(() => {
+        timeLeft--;
+        timerElement.textContent = `Time Left: ${timeLeft}`;
+
+    if (timeLeft === 0) {
+        clearInterval(gameInterval);
+        let timeLeft = 30;
+        alert(`Game Over!`);
+        startbtn.disabled = false;
+    }
+}, 1000)
+}
+
