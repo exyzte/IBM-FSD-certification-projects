41407836911c3f21ce54f74a677dc35ddaa4e6a0
exyzte <davidecicerys@gmail.com>
2025-08-14 19:46:03 -0500

    updated news_article

--- newsArticle_JSON/news_article.js
+++ newsArticle_JSON/news_article.js
@@ -5,47 +5,50 @@ xhr.open('GET', url, true);
 xhr.responseType = 'json';
 
 xhr.onload = function() {
-    var articles = xhr.response.articles;
-    var articlesDiv = document.getElementById('articles');
+    if (xhr.response && xhr.response.articles) { // Check if response and articles exist
+        var articles = xhr.response.articles;
+        var articlesDiv = document.getElementById('articles');
 
-    articles.forEach(function(article) {
-        var articleDiv = document.createElement('div');
-        articleDiv.classList.add('article');
+        articles.forEach(function(article) {
+            var articleDiv = document.createElement('div');
+            articleDiv.classList.add('article');
 
-        var title = document.createElement('h2');
-        title.textContent = article.title;
+            var title = document.createElement('h2');
+            title.textContent = article.title;
 
-        var description = document.createElement('p');
-        description.textContent = article.description;
-        description.style.fontWeight = 'bold';
+            var description = document.createElement('p');
+            description.textContent = article.description;
+            description.style.fontWeight = 'bold';
 
-        var source = document.createElement('p');
-        source.textContent = article.source;
-        source.style.fontStyle = 'italic';
+            var source = document.createElement('p');
+            source.textContent = article.source;
+            source.style.fontStyle = 'italic';
 
-        var image = document.createElement('img');
-        image.src = article.image_url;
-        image.alt = article.title;
-        image.style.width = '100px';
-        image.style.height = '100px';
+            var image = document.createElement('img');
+            image.src = article.image_url;
+            image.alt = article.title;
+            image.style.width = '100px';
+            image.style.height = '100px';
 
-        var publicationDate = document.createElement('p');
-        publicationDate.textContent = article.publication_date;
-        publicationDate.style.fontStyle = 'italic';
+            var publicationDate = document.createElement('p');
+            publicationDate.textContent = article.publication_date;
+            publicationDate.style.fontStyle = 'italic';
 
-        var content = document.createElement('p');
-        content.textContent = article.content;
+            var content = document.createElement('p');
+            content.textContent = article.content;
 
-        articleDiv.appendChild(title);
-        articleDiv.appendChild(description);
-        articleDiv.appendChild(source);
-        articleDiv.appendChild(image);
-        articleDiv.appendChild(publicationDate);
-        articleDiv.appendChild(content);
+            articleDiv.appendChild(title);
+            articleDiv.appendChild(description);
+            articleDiv.appendChild(source);
+            articleDiv.appendChild(image);
+            articleDiv.appendChild(publicationDate);
+            articleDiv.appendChild(content);
 
-        articlesDiv.appendChild(articleDiv);
-    });
-
+            articlesDiv.appendChild(articleDiv);
+        });
+    } else {
+        console.error("No articles found in the response.");
+    }
 };
 
 xhr.send();
